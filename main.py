5413572ff1e392338452488bd846c6f5d4527168
prasetiotriharyadi18-star <prasetiotriharyadi18@gmail.com>
2026-03-13 11:18:34 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from book import Book
 
 buku1 = Book(1, "Belajar Python", "Affan", 2025)
+buku2 = Book(1, "Belajar java", "budi", 2025)
 
 print(buku1.judul)
+print(buku1.get_update_terakhir())
+
+buku1.update_judul("Belajar Python Part I")
+
+print(buku1.judul)
+print(buku1.get_update_terakhir())
